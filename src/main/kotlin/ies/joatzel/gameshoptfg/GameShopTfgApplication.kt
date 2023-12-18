195dd13c91cf4a01953767368c16c1ac8f59c57c
Daniel Rodríguez Muñoz <daniel.ro.mu02@gmail.com>
2023-12-18 12:58:15 +0100

    Initial commit

---
+++ src/main/kotlin/ies/joatzel/gameshoptfg/GameShopTfgApplication.kt
@@ -0,0 +1,11 @@
+package ies.joatzel.gameshoptfg
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GameShopTfgApplication
+
+fun main(args: Array<String>) {
+    runApplication<GameShopTfgApplication>(*args)
+}
---
+++ src/test/kotlin/ies/joatzel/gameshoptfg/GameShopTfgApplicationTests.kt
@@ -0,0 +1,13 @@
+package ies.joatzel.gameshoptfg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GameShopTfgApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
